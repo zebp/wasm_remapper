d8c07e8679b3ce24e03c841a5a00e9651b4a11a6
vlakreeh <zeb@zebulon.dev>
2020-08-25 12:34:43 -0400

    Add some test cases

--- src/lib.rs
+++ src/lib.rs
@@ -29,28 +29,32 @@ pub struct RemapperBuilder<'wasm> {
 
 impl<'wasm> RemapperBuilder<'wasm> {
     /// The binary representation of the wasm that should be remapped.
-    pub fn input(&mut self, input: &'wasm [u8]) {
+    pub fn input(mut self, input: &'wasm [u8]) -> Self {
         self.input = Some(input);
+        self
     }
 
     /// A wasm binary with debug names included that can be used as a reference.
-    pub fn reference(&mut self, reference: &'wasm [u8]) {
+    pub fn reference(mut self, reference: &'wasm [u8]) -> Self {
         self.reference = Some(reference);
+        self
     }
 
     /// If constants that appear to be pointers into a wasm's data section should
     /// be ignored when comparing if two instructions match. Enabled by default.
-    pub fn ingore_constant_data_section_pointers(&mut self, enabled: bool) {
+    pub fn ingore_constant_data_section_pointers(mut self, enabled: bool) -> Self {
         self.ingore_constant_data_section_pointers = enabled;
+        self
     }
 
     /// If two functions need to have the exact same locals for them to be considered
     /// a potential match. Enabled by default.
-    pub fn require_exact_function_locals(&mut self, enabled: bool) {
+    pub fn require_exact_function_locals(mut self, enabled: bool) -> Self {
         self.require_exact_function_locals = enabled;
+        self
     }
 
-    pub fn remap() -> Result<RemapperOutput, RemapperError> {
+    pub fn remap(&self) -> Result<RemapperOutput, RemapperError> {
         todo!()
     }
 }
@@ -62,9 +66,48 @@ pub struct RemapperOutput {
 }
 
 #[derive(Debug, Error)]
-pub enum RemapperError {}
+pub enum RemapperError {
+    #[error("input wasm not a valid wasm binary")]
+    InvalidInputBinary,
+    #[error("reference wasm not a valid wasm binary")]
+    InvalidReferenceBinary,
+}
 
 #[cfg(test)]
 mod tests {
-    // TODO: Layout the api structure in the tests.
+
+    use super::*;
+    use std::fs;
+
+    fn read_wasm(name: &str) -> Vec<u8> {
+        fs::read(format!("test-cases/{}.wasm", name)).expect("unable to open test wasm")
+    }
+
+    #[test]
+    fn test_invalid_input() {
+        let reference = read_wasm("empty");
+        let result = Remapper::builder()
+            .input(&[])
+            .reference(&reference)
+            .remap();
+
+        match result {
+            Err(RemapperError::InvalidInputBinary) => {},
+            _ => panic!("unexpected result")
+        }
+    }
+
+    #[test]
+    fn test_invalid_empty_reference() {
+        let input = read_wasm("empty");
+        let result = Remapper::builder()
+            .input(&input)
+            .reference(&[])
+            .remap();
+
+        match result {
+            Err(RemapperError::InvalidReferenceBinary) => {},
+            _ => panic!("unexpected result")
+        }
+    }
 }
