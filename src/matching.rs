67102a82a24285d618398ec5726b8ab4b10efafc
vlakreeh <zeb@zebulon.dev>
2020-08-27 15:52:54 -0400

    Refactor MatchingContext so it can be reused on multiple input functions

--- src/matching.rs
+++ src/matching.rs
@@ -5,33 +5,38 @@ use crate::{
 use parity_wasm::elements::Instruction;
 
 pub struct MatchingContext<'a, 'wasm> {
-    input: &'a Function,
     data_regions: &'a [DataRegion],
     options: &'a Remapper<'wasm>,
 }
 
 impl<'a, 'wasm> MatchingContext<'a, 'wasm> {
-    pub fn new(
-        input: &'a Function,
-        data_regions: &'a [DataRegion],
-        options: &'a Remapper<'wasm>,
-    ) -> Self {
+    pub fn new(data_regions: &'a [DataRegion], options: &'a Remapper<'wasm>) -> Self {
         Self {
-            input,
             data_regions,
             options,
         }
     }
 
-    pub fn get_match_weight_for(&self, other: &Function) -> f32 {
-        let input_instructions = &self.input.instructions;
+    pub fn find_matches<'func>(
+        &self,
+        input: &Function,
+        others: &'func [Function],
+    ) -> Vec<(&'func Function, f32)> {
+        others
+            .into_iter()
+            .map(|other| (other, self.get_match_weight_for(input, other)))
+            .collect()
+    }
+
+    fn get_match_weight_for(&self, input: &Function, other: &Function) -> f32 {
+        let input_instructions = &input.instructions;
         let other_instructions = &other.instructions;
         let max_instructions = input_instructions.len().max(other_instructions.len());
 
-        if !self.does_signiture_match(other) {
+        if !self.does_signiture_match(input, other) {
             return 0.0;
         } else if self.options.require_exact_function_locals
-            && self.input.local_types != other.local_types
+            && input.local_types != other.local_types
         {
             return 0.0;
         }
@@ -50,8 +55,8 @@ impl<'a, 'wasm> MatchingContext<'a, 'wasm> {
         todo!()
     }
 
-    fn does_signiture_match(&self, other: &Function) -> bool {
-        let input = self.input;
+    fn does_signiture_match(&self, input: &Function, other: &Function) -> bool {
+        let input = input;
         input.param_types == other.param_types && input.return_type == other.return_type
     }
 }
